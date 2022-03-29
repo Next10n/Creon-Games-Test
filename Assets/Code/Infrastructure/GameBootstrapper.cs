bb0e5e196a0c58ad4100439441e857ccd644e496
next10n <ng_mail@mail.ru>
2022-03-29 14:51:34 +0700

    Mesh and MeshCutter
    Mesh and MeshCutter

---
+++ Assets/Code/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Code.Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private GameStateMachine _gameStateMachine;
+
+        private void Awake()
+        {
+            _gameStateMachine.Enter<BootstrapState>();
+        }
+    }
+}
